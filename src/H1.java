8b4824da47d2f9bd6454df0d1c6e1b285ee6f513
dmitr123456789 <114424493+dmitr123456789@users.noreply.github.com>
2022-10-02 20:50:18 +0300

    Update H1.java

--- src/H1.java
+++ src/H1.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class H1 {
+import javax.swing.*;
+
+public class H1 {
+    public static void main(String[] args) {
+        String number;
+        number = JOptionPane.showInputDialog("Введите трехзначное число:");
+        int a = Integer.parseInt(number);
+        int resulte = a / 100;
+        System.out.println(resulte);
+
+        double resulte1 = (double) a / 100;
+        System.out.println(Math.ceil(resulte1));
+    }
 }
