5d30b61ebee7b8b43f20145dfbd29ead4c242fc5
dmitr123456789 <114424493+dmitr123456789@users.noreply.github.com>
2022-10-02 20:51:05 +0300

    Update H2.java

--- src/H2.java
+++ src/H2.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class H2 {
+public class H2 {
+    public static void main(String[] args){
+int x1=1, y1=1;
+int x2=2, y2=2;
+boolean king = (x2==x1+1) && (y2==y1+1);
+        System.out.println(king);
+    }
 }
