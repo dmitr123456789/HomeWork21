4dbad96a0f113f10d0dfe35d7210bf4901285f23
dmitr123456789 <114424493+dmitr123456789@users.noreply.github.com>
2022-10-02 17:38:49 +0300

    Initial commit

---
+++ src/H3.java
@@ -0,0 +1,8 @@
+public class H3{
+    public static void main(String[] args){
+        int x1=1, y1=1;
+        int x2=2, y2=2;
+        boolean horse = x2!=x1+1 && y2!=y1+1;
+                System.out.println(horse);
+    }
+}
